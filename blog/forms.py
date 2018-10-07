668db1b1b9770b6ceb56d76c05a020d9d83029ba
ngockatz <ngoc1260@gmail.com>
2018-10-07 17:24:32 -0400

    Styled navbar and more

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from blog.models import Post, Comment
 
 
-class PostForm(forms.ModelForm):
+class PostForm(forms.ModelForm):  # render post_form.html by default
     class Meta:
         model = Post
         fields = ('author', 'title', 'text')
